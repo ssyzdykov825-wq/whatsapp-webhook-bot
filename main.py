571fe7d61d113bef4dccc4a526673661b6961ec8
ssyzdykov825-wq <ssyzdykov825@gmail.com>
2025-08-04 23:28:02 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from flask import Flask, request
-import os
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST', 'HEAD'])
 def webhook():
     if request.method == 'GET':
-        return "Webhook is running!"
+        return "Webhook is running!", 200
     elif request.method == 'POST':
         data = request.json
         print("Received message:", data)
         return "OK", 200
+    elif request.method == 'HEAD':
+        return '', 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run(host='0.0.0.0', port=5000)
