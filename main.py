95c3872ca4e9aa6d4b805bf7cfb869040c0b8f06
ssyzdykov825-wq <ssyzdykov825@gmail.com>
2025-08-04 22:53:01 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def webhook():
+    if request.method == 'GET':
+        return "Webhook is running!"
+    elif request.method == 'POST':
+        data = request.json
+        print("Received message:", data)
+        return "OK", 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
