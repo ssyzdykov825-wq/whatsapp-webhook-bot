88461eb38185f5e755fa2d2868fac4d53a845723
ssyzdykov825-wq <ssyzdykov825@gmail.com>
2025-08-05 23:57:44 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from flask import Flask, request
 
-app = Flask(__name__)
+app = Flask(name)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    data = request.get_json()
-    print("📩 Пришло сообщение от WhatsApp:", data)
+    print("🔔 POST-запрос получен")
+    try:
+        data = request.get_json(force=True)
+        print("📩 JSON:", data)
+    except Exception as e:
+        print("❌ Ошибка при получении JSON:", str(e))
     return "ok", 200
 
 @app.route('/')
 def index():
     return "Webhook is running!", 200
 
-if __name__ == "__main__":
+if name == "main":
     app.run(host='0.0.0.0', port=5000)
