6f0e0bf7d3de0638404405e8525531044dc5ca75
ssyzdykov825-wq <ssyzdykov825@gmail.com>
2025-08-07 15:45:26 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import requests
 from flask import Flask, request, jsonify
 from openai import OpenAI
-from fsm_healvix_kz import router
+import fsm_healvix_kz  # <-- тут теперь правильный импорт
 
 app = Flask(__name__)
 client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
